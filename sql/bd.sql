6efe4cbb17c7bb98f95c19316f1b582d99c7323b
aphexrage <aphexragedev@gmail.com>
2025-06-02 16:14:05 -0300

    Criando tabela produto e clientes e organizando a infra do projeto

--- sql/bd.sql
+++ sql/bd.sql
@@ -1,2 +1,17 @@
 CREATE database solus;
 USE solus
+
+CREATE TABLE clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    sobrenome VARCHAR(50),
+    cpf CHAR(11) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE produto (
+	id_produto INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR (50) NOT NULL,
+    categoria VARCHAR (50) NOT NULL,
+    preco DECIMAL (10,2) NOT NULL
+)
